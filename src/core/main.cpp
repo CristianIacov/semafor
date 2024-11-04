043eca8abb48205c3f494fbecccc1cc312c72a05
Cristian Iacov <cristian.iacov1998@yahoo.com>
2024-11-04 23:44:01 +0200

    threading

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,7 +1,27 @@
 #include "semafor/Semafor.hpp"
-
+#include <thread>
+#include <iostream>
+static bool isFinished = false;
+void doSomething()
+{
+    while(!isFinished)
+    {
+        std::cout << "this thread is runnnning" << std::endl;
+    }
+}
 int main(int argc, char **argv)
 {
     Semafor semafor;
+    std::thread secondThread(doSomething);
+
+    std::cin.get();
+
+    isFinished = true;
+
+    secondThread.join();
+
+    std::cin.get();
+
+    std::cout << "Finishing" << std::endl;
     return 0;
 }
